6b54a8a126d73a66ab363a648da94ddf706ee2e2
Alessandro Carta <35343000+alexpaper@users.noreply.github.com>
2020-07-28 22:17:58 +0200

    Add speech recognition

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
+// Check system preference
 const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
-if (prefersDarkScheme.matches) { document.body.classList.add('dark') } else { document.body.classList.remove('dark') }
+if (prefersDarkScheme.matches) {
+    document.body.classList.add('dark')
+} else {
+    document.body.classList.remove('dark')
+}
 
-// Select check box
+// Select elements
 const dtoggle = document.querySelector('.check');
 const label = document.querySelector('label');
 const moon = document.querySelector('.moon');
 const sun = document.querySelector('.sun');
+const title = document.querySelector('.title');
+
 // Check if dark class is applied
 let active = document.body.classList.contains('dark');
-console.log(active)
+// console.log(active)
 if (active) {
     label.innerHTML = 'Light';
     sun.classList.add('transparent')
@@ -18,31 +25,99 @@ if (!active) {
     label.innerHTML = 'Dark';
     sun.classList.remove('transparent')
 }
-// check.checked = true;
-// dtoggle.disabled = false;
+// Checkbox event listener
 dtoggle.addEventListener('click', () => {
-    console.log(dtoggle.checked)
-        // Change label text
+    // console.log(dtoggle.checked)
+    // Change label text
     if (dtoggle.checked) {
         label.innerHTML = 'Light';
         sun.classList.add('transparent')
-            // dtoggle.checked = true;
+        // dtoggle.checked = true;
         dtoggle.click();
     }
     if (!dtoggle.checked) {
         label.innerHTML = 'Dark';
         sun.classList.remove('transparent')
-            // Use Click method
+        // Use Click method
         dtoggle.click()
     }
     // Toggle dark theme
     document.body.classList.toggle('dark')
 });
 
+
+// Speech command
+// Hide speech info after 6s
+const speechInfo = document.querySelector('.speech-info');
+const recBtn = document.querySelector('.rec');
+// Set Time out
+
+setTimeout(() => {
+    speechInfo.style.opacity = 0;
+}, 6000);
+
+// Speech recognition
+const recognition = new window.webkitSpeechRecognition() || window.webkitSpeechRecognition;
+recognition.continuous = false;
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+// On result event
+recognition.onresult = e => {
+    if (typeof (e.results) == 'undefined') {
+        recognition.onend = null;
+        recognition.stop();
+        return;
+    }
+    let mode = e.results[0][0].transcript;
+    if (mode == 'dark') {
+        // Check if dark mode is activated
+        let active = document.body.classList.contains('dark');
+        if (active) {
+            // if activated do nothing
+            recognition.onend = null;
+            recognition.stop();
+            return;
+        } else {
+            label.innerHTML = 'Light';
+            sun.classList.add('transparent');
+            // Toggle checkbox
+            dtoggle.click();
+            // Change title
+            title.innerHTML = 'Dark Mode';
+        }
+    }
+    if (mode == 'light') {
+        // Check if dark mode is activated
+        let active = document.body.classList.contains('dark');
+        if (!active) {
+            // if no activated do nothing
+            recognition.onend = null;
+            recognition.stop();
+            return;
+        } else {
+            label.innerHTML = 'Dark';
+            sun.classList.remove('transparent');
+            // Toggle checkbox
+            dtoggle.click();
+            // Change title
+            title.innerHTML = 'Light Mode';
+        }
+    }
+    recognition.onnomatch = e => {
+        recognition.stop();
+    }
+}
+
+// On click star recognition
+recBtn.onclick = () => {
+    recognition.start();
+}
+
 // Paragraph
 let paragraph = document.querySelector('.text')
-    //console.log(paragraph.innerHTML)
-    //console.log(paragraph.innerHTML.length)
+//console.log(paragraph.innerHTML)
+//console.log(paragraph.innerHTML.length)
 let str = paragraph.innerText;
 // Insert New Line Function
 function newLine(str, maxLength) {
@@ -55,7 +130,9 @@ function newLine(str, maxLength) {
         // Add text string to line
         line += str.substr(i * maxLength, maxLength);
         // Brake
-        if (i !== numOfLines) { line += "\n "; }
+        if (i !== numOfLines) {
+            line += "\n ";
+        }
     }
 
     // return buff;
@@ -64,3 +141,5 @@ function newLine(str, maxLength) {
 }
 // Run
 newLine(str, 50)
+
+// Bye ...
--- readme.md
+++ readme.md
@@ -4,13 +4,37 @@ Simple javascript darkmode page.
 ![DarkMode](img/darkmode.gif)
 
 The dark mode can be enabled usign the checkbox, by default the page read the system preferences and automatically set the page in dark or lighy mode. 
+You can enable dark mode using also speech-recognition.
 ## Stack
 
 Html
 Css
 Javascript
 
-### License MIT
+## Run
+To run this project you can open the index.html file or use [http-server](https://www.npmjs.com/package/http-server)
 
-@lexpaper
+- You need to have [Node.js](https://nodejs.org/) installed in your system.
+
+- Open a CMD or Terminal prompt, run the command:
+```
+ npm install http-server -g
+```
+
+- Navigate to the specific path of your file folder in CMD or Terminal, and run the command 
+```
+http-server
+```
+
+- Go to your browser and type 
+```
+localhost:8080
+```
+Your Application should run there.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+
+author ❤️ @lexpaper
+
 
